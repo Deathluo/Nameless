eda8259666ec24bc1bfb635d0742186e92caa39e
helei <hl1282456555@outlook.com>
2019-01-03 16:25:52 +0800

    Setup project config and create basic file.

---
+++ Source/Nameless/NamelessGameInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "NamelessGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAMELESS_API UNamelessGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/Nameless/Public/LoginMapGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LoginMapGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAMELESS_API ALoginMapGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
